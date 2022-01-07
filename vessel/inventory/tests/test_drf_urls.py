6f235dfcb3b1e2a16708e4a0d0a61f617273eaf1
pcampos <pcampos119104@gmail.com>
2022-01-07 10:06:42 -0300

    testing telegram message

--- vessel/inventory/tests/test_drf_urls.py
+++ vessel/inventory/tests/test_drf_urls.py
@@ -1,20 +1,15 @@
 import pytest
 from django.urls import resolve, reverse
 
-
 pytestmark = [pytest.mark.django_db, pytest.mark.inventory]
 
 
 class TestItemUrls:
     def test_detail(self, item) -> None:
         assert (
-            reverse("api:item-detail", args=(item.pk,))
-            == f"/api/inventory/{item.pk}/"
+            reverse("api:item-detail", args=(item.pk,)) == f"/api/inventory/{item.pk}/"
         )
-        assert (
-            resolve(f"/api/inventory/{item.pk}/").view_name
-            == "api:item-detail"
-        )
+        assert resolve(f"/api/inventory/{item.pk}/").view_name == "api:item-detail"
 
     def test_list(self) -> None:
         assert reverse("api:item-list") == "/api/inventory/"
--- vessel/inventory/tests/test_views.py
+++ vessel/inventory/tests/test_views.py
@@ -13,9 +13,7 @@ class TestInventoryEndpoints:
     endpoint = "/api/inventory/"
 
     def test_list(self, api_client):
-        baker.make(
-            "inventory.Item", _quantity=3, _fill_optional=True, is_removed=False
-        )
+        baker.make("inventory.Item", _quantity=3, _fill_optional=True, is_removed=False)
 
         response = api_client.get(self.endpoint)
 
@@ -23,9 +21,7 @@ class TestInventoryEndpoints:
         assert len(json.loads(response.content)) == 3
 
     def test_create(self, api_client):
-        item = baker.prepare(
-            "inventory.Item", _fill_optional=True, is_removed=False
-        )
+        item = baker.prepare("inventory.Item", _fill_optional=True, is_removed=False)
         expected_json = {"name": item.name, "qtd": item.qtd}
 
         response = api_client.post(self.endpoint, data=expected_json, format="json")
@@ -35,9 +31,7 @@ class TestInventoryEndpoints:
         assert_contains(response, expected_json["qtd"], status_code=201)
 
     def test_retrieve(self, api_client):
-        item = baker.make(
-            "inventory.Item", _fill_optional=True, is_removed=False
-        )
+        item = baker.make("inventory.Item", _fill_optional=True, is_removed=False)
         expected_json = {
             "pk": item.pk,
             "name": item.name,
@@ -53,9 +47,7 @@ class TestInventoryEndpoints:
         assert_contains(response, expected_json["qtd"])
 
     def test_update(self, rf, api_client):
-        old_item = baker.make(
-            "inventory.Item", _fill_optional=True, is_removed=False
-        )
+        old_item = baker.make("inventory.Item", _fill_optional=True, is_removed=False)
         new_item = baker.prepare(
             "inventory.Item", _fill_optional=True, is_removed=False
         )
@@ -77,9 +69,7 @@ class TestInventoryEndpoints:
         ],
     )
     def test_partial_update(self, field, api_client):
-        item = baker.make(
-            "inventory.Item", _fill_optional=True, is_removed=False
-        )
+        item = baker.make("inventory.Item", _fill_optional=True, is_removed=False)
         item_dict = {
             "pk": item.pk,
             "name": item.name,
@@ -95,9 +85,7 @@ class TestInventoryEndpoints:
         assert json.loads(response.content)[field] == valid_field
 
     def test_delete(self, api_client):
-        item = baker.make(
-            "inventory.Item", _fill_optional=True, is_removed=False
-        )
+        item = baker.make("inventory.Item", _fill_optional=True, is_removed=False)
         url = f"{self.endpoint}{item.pk}/"
 
         response = api_client.delete(url)
