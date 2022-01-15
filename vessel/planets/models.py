b4e8beee6cd1ba4e02994c54554d31add1a1eadc
Francisco V <vxrnxk@gmail.com>
2022-01-15 08:39:21 -0300

    planet age calculation

--- vessel/planets/models.py
+++ vessel/planets/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date
 
 from django.db import models
 
@@ -6,19 +6,27 @@ from vessel.core.models import ModelBase
 
 
 class Planet(ModelBase):
-    name = models.CharField(max_length=60, verbose_name="Name")
+    name = models.CharField(
+        max_length=60,
+        verbose_name="Name"
+    )
     total_area = models.DecimalField(
-        verbose_name="Total Area", max_digits=9, decimal_places=2
+        verbose_name="Total Area",
+        max_digits=9,
+        decimal_places=2
     )
     population = models.IntegerField(
         verbose_name="Population",
     )
-    discovery_date = models.DateTimeField(verbose_name="Discovery Date", blank=True)
+    discovery_date = models.DateTimeField(
+        verbose_name="Discovery Date",
+        blank=True
+    )
 
     @property
     def age(self):
-        age = self.discovery_date - datetime.datetime.now()
-        return age
+        today = date.today()
+        return today.year - self.discovery_date.year - ((today.month, today.day) < (self.discovery_date.month, self.discovery_date.day))
 
     class Meta:
         verbose_name = "Planet"
