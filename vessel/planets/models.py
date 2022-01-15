aace5edfae8b3264671fe7eb61f54a55429c3b7e
Francisco V <vxrnxk@gmail.com>
2022-01-15 08:41:10 -0300

    pep8 black

--- vessel/planets/models.py
+++ vessel/planets/models.py
@@ -6,27 +6,26 @@ from vessel.core.models import ModelBase
 
 
 class Planet(ModelBase):
-    name = models.CharField(
-        max_length=60,
-        verbose_name="Name"
-    )
+    name = models.CharField(max_length=60, verbose_name="Name")
     total_area = models.DecimalField(
-        verbose_name="Total Area",
-        max_digits=9,
-        decimal_places=2
+        verbose_name="Total Area", max_digits=9, decimal_places=2
     )
     population = models.IntegerField(
         verbose_name="Population",
     )
-    discovery_date = models.DateTimeField(
-        verbose_name="Discovery Date",
-        blank=True
-    )
+    discovery_date = models.DateTimeField(verbose_name="Discovery Date", blank=True)
 
     @property
     def age(self):
         today = date.today()
-        return today.year - self.discovery_date.year - ((today.month, today.day) < (self.discovery_date.month, self.discovery_date.day))
+        return (
+            today.year
+            - self.discovery_date.year
+            - (
+                (today.month, today.day)
+                < (self.discovery_date.month, self.discovery_date.day)
+            )
+        )
 
     class Meta:
         verbose_name = "Planet"
